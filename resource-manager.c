f2a00924e6c6061c98c9888774b70430063f7876
Jason Yao <jasony.edu@gmail.com>
2015-11-18 16:58:13 -0500

    Added C boilerplate

---
+++ resource-manager.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef enum {false, true} bool;        // Allows boolean types in C
+
+int main(int argc, char *argv[])
+{
+
+} // End of the main function
