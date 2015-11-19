45348073fde82286ac8c49d65e1b26cde974f3a0
Jason Yao <jasony.edu@gmail.com>
2015-11-19 13:05:10 -0500

    Added input validation

--- resource-manager.c
+++ resource-manager.c
@@ -4,7 +4,52 @@
 
 typedef enum {false, true} bool;        // Allows boolean types in C
 
+bool IS_VERBOSE_MODE; // Flags whether the output should be detailed or not
+
+/**
+ * Sets global flags for output depending on user input
+ * @param argc The number of arguments in argv, where each argument is space deliminated
+ * @param argv The command used to run the program, with each argument space deliminated
+ */
+uint8_t setFlags(int32_t argc, char *argv[])
+{
+    if (argc == 2)
+        return 1;
+    else if (argc == 3)
+    {
+        if (strcmp(argv[1], "--verbose") == 0)
+        {
+            IS_VERBOSE_MODE = true;
+            return 2;
+        }
+        else
+        {
+            fprintf(stderr, "Error: Invalid commandline argument formatting!\n");
+            exit(1);
+        }
+    }
+    else
+    {
+        fprintf(stderr, "Error: Invalid commandline argument formatting!\n");
+        exit(1);
+    }
+} // End of the setFlags function
+
 int main(int argc, char *argv[])
 {
+    // Reads in from file
+    FILE* inputFile;
+    char* filePath;
 
+    filePath = argv[setFlags(argc, argv)]; // Sets any global flags from input
+
+    inputFile = fopen(filePath, "r");
+
+    // [ERROR CHECKING]: INVALID FILENAME
+    if (inputFile == NULL) {
+        fprintf(stderr, "Error: cannot open input file %s!\n", filePath);
+        exit(1);
+    }
+    
+    return EXIT_SUCCESS;
 } // End of the main function
