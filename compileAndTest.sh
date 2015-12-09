aa38de431b17ae610d2071afb3f2ba70989b6681
Jason Yao <jasony.edu@gmail.com>
2015-12-09 15:32:53 -0500

    Added working java compile and test script

--- compileAndTest.sh
+++ compileAndTest.sh
@@ -1,4 +1,13 @@
-testNumber="08"
-javac src/* out/
-java out/resource-manager --verbose testing/input/input-$testNumber
+#!/bin/bash -e
+
+testNumber="05"
+
+# Checks whether or not the output file is created, creates one if not
+if [ ! -d output ]; then
+	mkdir output
+	echo "Creating output directory"
+fi
+
+javac src/*.java -d output
+java -cp output SimulateResourceManagers --verbose testing/input/input-$testNumber
 cat testing/output/output-$testNumber
